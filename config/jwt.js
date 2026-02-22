4063f30b0a6f037bc4bc76a445305d86f20d89d5
SumanChahal <sumochahal2510@gmail.com>
2026-02-22 12:17:26 +0530

    Updated jwt.js with JWT_SECRET fix

--- config/jwt.js
+++ config/jwt.js
@@ -9,3 +9,22 @@ exports.generateToken = (payload) => {
 exports.verifyToken = (token) => {
   return jwt.verify(token, process.env.JWT_SECRET);
 };
+// ...existing code...
+// const jwt = require("jsonwebtoken");
+// require("dotenv").config();
+
+// const SECRET = process.env.JWT_SECRET;
+// if (!SECRET) {
+//   throw new Error("JWT_SECRET environment variable is not set");
+// }
+
+// exports.generateToken = (payload) => {
+//   return jwt.sign(payload, SECRET, {
+//     expiresIn: "7d",
+//   });
+// };
+
+// exports.verifyToken = (token) => {
+//   return jwt.verify(token, SECRET);
+// };
+// ...existing code...
